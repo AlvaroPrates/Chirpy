1183cb4d90cf137f13f816e5598847f7408252a7
AlvaroPrates <alvarobirckprates@gmail.com>
2024-06-06 19:36:08 -0300

    the profane WIP

--- main.go
+++ main.go
@@ -18,12 +18,16 @@ func main() {
 	}
 
 	mux := http.NewServeMux()
-	mux.Handle("/app/*", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
-	mux.HandleFunc("GET /api/healthz", handlerReadiness)
-	mux.HandleFunc("GET /admin/metrics", apiCfg.handleMetrics)
-	mux.HandleFunc("/api/reset", apiCfg.resetMetrics)
+	fsHandler := apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.Handle("/app/*", fsHandler)
 
-	srv := http.Server{
+	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+	mux.HandleFunc("GET /api/reset", apiCfg.resetMetrics)
+	mux.HandleFunc("POST /api/validate_chirp", handleValidateChirps)
+
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handleMetrics)
+
+	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
